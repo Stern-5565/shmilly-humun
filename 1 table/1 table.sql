e67dd13fe5e9fc96b8179f2d6e7f98865f7dc439
Stern-5565 <shmillystern@gmail.com>
2025-09-11 16:47:21 +0100

    made the num coulmn nullable

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,4 +1,6 @@
 use shmillyhumun
 go 
+drop table if exists body
+go
 create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null, num int not null)
+bodypart varchar(100) not null, num int  null)
